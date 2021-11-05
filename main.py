13b1053b5968532b49b9daaa68284bd4eb227425
lifltr <91332921+lifltr@users.noreply.github.com>
2021-11-05 17:54:41 +0100

    erster Entwurf

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask("templates")
+
+
+@app.route("/hello/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        ziel_person = request.form['vorname']
+        rueckgabe_string = "<li> " + ziel_person + "</li>"
+
+        return render_template("Serie_angelegt.html", ansprechperson=rueckgabe_string)
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
